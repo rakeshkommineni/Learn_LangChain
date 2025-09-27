139d602e36d14c74fba7de6a6ca7d5b91ef00fba
KommineniRakesh <rakeshkommineni147@gmail.com>
2025-09-27 15:14:32 -0500

    sample chat model

---
+++ Create_Chat_Models/your_first_model.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4")
+
+result = llm.invoke("What is the current time in India?")
+
+print(result)
