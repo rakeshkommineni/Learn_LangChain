4be578c03eae7f0839c654342f2e2e81bb5f6893
KommineniRakesh <rakeshkommineni147@gmail.com>
2025-09-27 15:25:30 -0500

    [LC-1231] Sample chat model with GPT-3.5-turbo

---
+++ Chat_Models/sample_model.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+result = llm.invoke("What is the current time in India?")
+
+print(result)
